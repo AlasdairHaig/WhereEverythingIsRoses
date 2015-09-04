5b50ca6a10df3151e50272f681197d5c76822130
AlasdairHaig <boaz.sylvannia@live.com>
2015-09-04 15:55:22 +0200

    Plantae Class
    Just preparing base plant class. Hoping to try some inheritance or
    aggregation.

---
+++ Plantae/Plantae/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plantae
+{
+    public class Class1
+    {
+        /* Properties of Plants:
+         * 
+         * Alive
+         * Dead
+         * Hunger
+         * Thirst
+         * Color
+         * Size : Height, Length, Width
+         */
+
+
+        /* Behaviors of Plants:
+         * 
+         * Eat
+         * Grow
+         * Drink
+         * Photosynthesis
+         */
+
+
+
+
+    }
+}
